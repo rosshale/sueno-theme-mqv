f793eedc8e9a8eb720ef705173bac13f4857830d
Michael Weber <msweber@gmail.com>
2013-08-16 07:10:41 +1000

    Added new image attachment dimensions.

--- sueno.js
+++ sueno.js
@@ -1,7 +1,15 @@
 $(document).ready(function(){
+
+	// Open all links externally that start with http
 	$("a[href^=http]").each(function(){
 		if(this.href.indexOf(location.hostname) == -1) {
 			$(this).attr({target: "_blank",});
 		}
-	})
+	});
+
+	// Fix attached width
+	var width = $('.attach-image').width() - 20;
+	$('.attach-image').css('max-height', 'auto');
+	$('.attach-image img').css('max-width', width + 'px');	
+
 });
